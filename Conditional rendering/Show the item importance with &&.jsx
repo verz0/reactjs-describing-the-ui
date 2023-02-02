d25aee1225d3cca012b96c11dc0bc2d535409292
Pirajesh <pirajeshmr@gmail.com>
2023-02-02 23:07:24 +0530

    added refactoring challenge

---
+++ Conditional rendering/Show the item importance with &&.jsx
@@ -0,0 +1,32 @@
+function Item({ name, importance }) {
+    return (
+      <li className="item">
+        {name} 
+        {importance > 0 && ' '}
+        {importance > 0 && <b>(Importance: {importance})</b>}
+      </li>
+    );
+  }
+  
+  export default function PackingList() {
+    return (
+      <section>
+        <h1>Sally Ride's Packing List</h1>
+        <ul>
+          <Item 
+            importance={9} 
+            name="Space suit" 
+          />
+          <Item 
+            importance={0} 
+            name="Helmet with a golden leaf" 
+          />
+          <Item 
+            importance={6} 
+            name="Photo of Tam" 
+          />
+        </ul>
+      </section>
+    );
+  }
+  
